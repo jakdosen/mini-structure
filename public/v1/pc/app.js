1dcb4c66359395a2bb30c68f75979bd8c226aa71
Gavin Li <liwirong@gmail.com>
2016-03-03 16:03:33 +0800

    new

--- public/v1/pc/app.js
+++ public/v1/pc/app.js
@@ -4,6 +4,7 @@
       anchors: ['page-1', 'page-2', 'page-3', 'page-4', 'page-5'],
       navigation: true,
       navigationPosition: 'right',
+
       onLeave: function(index, nextIndex){
           $("#menu>.nav_item:eq(" + --index +")").removeClass("active");
           $("#menu>.nav_item:eq(" + --nextIndex + ")").addClass("active");
